39917d919c5e1c17ccd3e201afe55c5bfbf71698
Mario <hristevmario@gmail.com>
2016-11-14 19:54:51 +0200

    initial commit with basic chat structure

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Chat system with php</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div id="container">
+		<div id="chat_box">
+			<div id="chat-data">
+				<span>Wally :</span>
+				<span>How are you ?</span>
+				<span>12:39 PM</span>
+			</div>
+		</div>
+		
+		<form action="" method="post">
+			<input type="text" name="name" placeholder="Your name">
+			<textarea name="enter message" id="" cols="30" rows="10" placeholder="Enter your message here"></textarea>
+			<input type="submit" value="Send it" name="submit">
+		</form>
+	</div>
+</body>
+</html>
